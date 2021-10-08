34e2c7e9c43dc8cec66ddac5873ac077316c1131
alawto01 <91485423+alawto01@users.noreply.github.com>
2021-10-08 11:02:39 -0400

    Add files via upload

---
+++ Homework3/Objects.java
@@ -0,0 +1,13 @@
+class Dog{
+	Dog(int age, String name){
+		System.out.print(name + " is " + age + " years old");
+	}
+}
+public class Objects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog dog = new Dog(2, "Ace"); 
+	}
+
+}
